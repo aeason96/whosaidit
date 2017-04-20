301dfb69c4b9fdf435644a93a7e0aaccd2492d48
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-04-19 22:39:54 -0400

    preliminary urls and more views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls import url
+from api.views import GameRoomCreateView, PlayerCreateView, QuestionCreateView, AnswerCreateView, AnswerUpdateView, \
+    AnswerListView, PlayerDestroyView
 
 urlpatterns = [
-
+    url(r'^gameroom/create/$', GameRoomCreateView.as_view()),
+    url(r'^player/create/$', PlayerCreateView.as_view()),
+    url(r'^question/create/$', QuestionCreateView.as_view()),
+    url(r'^answer/create/$', AnswerCreateView.as_view()),
+    url(r'^answer/update/$', AnswerUpdateView.as_view()),
+    url(r'^answers/$', AnswerListView.as_view()),
+    url(r'^player/delete/$', PlayerDestroyView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -21,3 +21,18 @@ class QuestionCreateView(generics.CreateAPIView):
 class AnswerCreateView(generics.CreateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+
+
+class AnswerUpdateView(generics.UpdateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class AnswerListView(generics.ListAPIView):
+    queryset = Answer.objects.all() # TODO we need to filter the answers based on GameRoom
+    serializer_class = AnswerSerializer
+
+
+class PlayerDestroyView(generics.DestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
