c116fb88ab4bf6fed5041091046bb8afcdecf8bd
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-04-24 22:39:10 -0400

    change to allow hosts

--- whosaidit/settings.py
+++ whosaidit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(fql@)x!bpp@bbo4a&+0k=5wx&zwrn+$f6%i#^a0)x!31j$2l('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
