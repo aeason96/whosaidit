c864aeb0d0133a525e393b04b6f9632d5a07ceac
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-05-02 13:51:48 -0400

    change back to the right db

--- whosaidit/settings.py
+++ whosaidit/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'whosaidit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 
 # Password validation
